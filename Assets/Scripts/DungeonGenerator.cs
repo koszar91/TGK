7634ed2febf072cdbb1ce0091c42bb167ac731f4
koszar91 <tkoszarek@student.agh.edu.pl>
2023-04-12 18:55:41 +0200

    Added walls generation on a separate tilemap.

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -12,12 +12,43 @@ public class DungeonGenerator : MonoBehaviour
     public Tilemap floorTilemap;
     public TileBase floorTile;
 
+    public Tilemap wallTilemap;
+    public TileBase wallTile;
+
     public void Start()
     {
         Generate();
     }
     
     private void Generate()
+    {
+        var floorPositions = GenerateFloorPositions();
+        var wallPositions = GenerateWallPositions(floorPositions);
+
+        VisualizeTiles(floorPositions, floorTilemap, floorTile);
+        VisualizeTiles(wallPositions, wallTilemap, wallTile);
+    }
+
+    private HashSet<Vector2Int> GenerateWallPositions(HashSet<Vector2Int> floorPositions)
+    {
+        var wallPositions = new HashSet<Vector2Int>();
+
+        foreach (var floorPosition in floorPositions)
+        {
+            foreach (var direction in ProceduralGeneration.Directions)
+            {
+                var potentialWallPosition = floorPosition + direction;
+                if (!floorPositions.Contains(potentialWallPosition))
+                {
+                    wallPositions.Add(potentialWallPosition);
+                }
+            }
+        }
+
+        return wallPositions;
+    }
+
+    private HashSet<Vector2Int> GenerateFloorPositions()
     {
         var floorPositions = new HashSet<Vector2Int>();
         var position = startPosition;
@@ -28,15 +59,15 @@ public class DungeonGenerator : MonoBehaviour
             floorPositions.UnionWith(walk);
         }
 
-        VisualizeFloor(floorPositions);
+        return floorPositions;
     }
 
-    private void VisualizeFloor(HashSet<Vector2Int> floorPositions)
+    private void VisualizeTiles(HashSet<Vector2Int> positions, Tilemap tilemap, TileBase tile)
     {
-        foreach (var position in floorPositions)
+        foreach (var position in positions)
         {
-            var tilePosition = floorTilemap.WorldToCell((Vector3Int)position);
-            floorTilemap.SetTile(tilePosition, floorTile);
+            var tilePosition = tilemap.WorldToCell((Vector3Int)position);
+            tilemap.SetTile(tilePosition, tile);
         }
     }
 }
