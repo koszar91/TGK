f13845deeb82565c9167e076a61dda66f16c5f27
koszar91 <tkoszarek@student.agh.edu.pl>
2023-04-12 16:59:21 +0200

    Init

---
+++ Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    float speed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        transform.position = transform.position + movement * Time.deltaTime * speed;
+    }
+}
