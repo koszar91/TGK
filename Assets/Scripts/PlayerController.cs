c077b9606bad74b77ff2dd502f4c0938b1ce101c
koszar91 <tkoszarek@student.agh.edu.pl>
2023-04-13 18:58:02 +0200

    Added following camera Added sprite flipping when moving left

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,17 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float movementSpeed = 1.0f;
-    public float collisionOffset = 0.02f;
+    public float collisionOffset = 0.005f;
     public ContactFilter2D movementFilter;
-    
     Vector2 movementInput;
+    
+    SpriteRenderer sprite;
     Rigidbody2D rb;
-    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     void FixedUpdate()
@@ -33,6 +34,7 @@ public class PlayerController : MonoBehaviour
         if (movementInput == Vector2.zero) return;
 
         // Check for vertical and horizontal collisions
+        List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
         int collisionsVerticalCount = rb.Cast(
             new Vector2(0.0f, movementInput.y), movementFilter, castCollisions,
             movementSpeed * Time.fixedDeltaTime + collisionOffset
@@ -46,7 +48,12 @@ public class PlayerController : MonoBehaviour
         var movement = new Vector2(0.0f, 0.0f);
         if (collisionsVerticalCount == 0)   movement += new Vector2(0.0f, movementInput.y);
         if (collisionsHorizontalCount == 0) movement += new Vector2(movementInput.x, 0.0f);
-        
+
         rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+
+        // Flip sprite according to moving direction
+        if (movement.x > 0) sprite.flipX = false;
+        if (movement.x < 0) sprite.flipX = true;
+        else {} // leave sprite as is
     }
 }
